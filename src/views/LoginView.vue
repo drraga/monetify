29e9b8af6cb3e14191792a8faaba344bb2705a0d
Petr Losev <geraltrivski@mail.ru>
2024-08-12 16:46:03 +0300

    Cleaning folders and assets.

---
+++ src/views/LoginView.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+// import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <!-- <TheWelcome /> -->
+    <div class="content"></div>
+  </main>
+</template>
+
+<style lang="scss">
+.content {
+  margin-inline-start: 23.438rem;
+}
+</style>
